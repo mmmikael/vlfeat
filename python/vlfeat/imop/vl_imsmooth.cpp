a7a1ef2272e206fa06fdac52ceb43f781a077744
Andy <amueller@ais.uni-bonn.de>
2010-10-22 13:42:05 +0200

    Resolved merging conflicts

--- python/vlfeat/imop/vl_imsmooth.cpp
+++ python/vlfeat/imop/vl_imsmooth.cpp
@@ -234,7 +234,7 @@ PyObject * vl_imsmooth_python(PyArrayObject & image, double sigma)
 #undef VL_IMCONVCOLTRI
 #define FLT float
 #define VL_IMCONVCOL vl_imconvcol_vf
-#define VL_IMCONVCOLTRI vl_imconvcoltri_vf
+#define VL_IMCONVCOLTRI vl_imconvcoltri_f
 #include "imsmooth.tc"
 //		break;
 //	case mxDOUBLE_CLASS:

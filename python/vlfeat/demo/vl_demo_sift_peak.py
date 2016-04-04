a5407cd905a5d1987cd763eab1166ee2e9b35580
Simmi Mourya <simmimourya@outlook.com>
2016-04-04 16:25:25 +0530

    Code clean up: styled according to PEP 8 Conventions

--- python/vlfeat/demo/vl_demo_sift_peak.py
+++ python/vlfeat/demo/vl_demo_sift_peak.py
@@ -1,38 +1,38 @@
-import Image
-import numpy
-import pylab
+""" VL_DEMO_SIFT_PEAK  Demo: SIFT: peak threshold """
+
+import numpy as np
+import matplotlib.pyplot as plt
 import vlfeat
 from vlfeat.plotop.vl_plotframe import vl_plotframe
 from numpy.random import uniform
 
 if __name__ == '__main__':
-	""" VL_DEMO_SIFT_PEAK  Demo: SIFT: peak treshold
-	"""
-	tmp = uniform(0, 1, (100, 500)) 
-	I = numpy.zeros([100, 500])
-	I.ravel()[pylab.find(tmp.ravel()<=0.005)] = 1
-	
-	I = (numpy.ones([100,1]) *  numpy.r_[0:1:500j]) * I 
-	I[:, 0] = 0
-	I[:, -1] = 0
-	I[0, :] = 0
-	I[-1, :] = 0
-	
-	I = numpy.array(I, 'f', order='F')
-	I = 2 * numpy.pi * 4**2 * vlfeat.vl_imsmooth(I, 4)	
-	I = 255 * I
 
-	print 'sift_peak_0'
+    tmp = uniform(0, 1, (100, 500))
+    I = np.zeros([100, 500])
+    I.ravel()[plt.find(tmp.ravel() <= 0.005)] = 1
 
-	I = numpy.array(I, 'f', order='F')
-	tpr = [0, 10, 20, 30]
-	for tp in tpr:
-		f, d = vlfeat.vl_sift(I, peak_thresh=tp, edge_thresh=10000)
-		
-		pylab.figure()
-		pylab.gray()
-		pylab.imshow(I)
-		vl_plotframe(f, color='k', linewidth=3)
-		vl_plotframe(f, color='y', linewidth=2)
-	
-	pylab.show()
+    I = (np.ones([100, 1]) * np.r_[0:1:500j]) * I
+    I[:, 0] = 0
+    I[:, -1] = 0
+    I[0, :] = 0
+    I[-1, :] = 0
+
+    I = np.array(I, 'f', order='F')
+    I = 2 * np.pi * 4 ** 2 * vlfeat.vl_imsmooth(I, 4)
+    I *= 255
+
+    print 'sift_peak_0'
+
+    I = np.array(I, 'f', order='F')
+    tpr = [0, 10, 20, 30]
+    for tp in tpr:
+        f, d = vlfeat.vl_sift(I, peak_thresh=tp, edge_thresh=10000)
+
+        plt.figure()
+        plt.gray()
+        plt.imshow(I)
+        vl_plotframe(f, color='k', linewidth=3)
+        vl_plotframe(f, color='y', linewidth=2)
+
+    plt.show()
--- python/vlfeat/kmeans/vl_ikmeanshist.py
+++ python/vlfeat/kmeans/vl_ikmeanshist.py
@@ -1,12 +1,12 @@
-import numpy
+import numpy as np
 import vlfeat
 
 def vl_ikmeanshist(K, asgn):
-	""" VL_IKMEANSHIST  Compute histogram of quantized data
-	  H = VL_IKMEANSHIST(K,ASGN) computes the histogram of the IKM clusters
-	  activated by cluster assignments ASGN.
-	"""
 
-	h = numpy.zeros(K, 'float64')
-	h = vl_binsum(h, numpy.array([1], 'float64'), numpy.array(asgn, 'float64'))
-	return h
+    """ VL_IKMEANSHIST  Compute histogram of quantized data
+        H = VL_IKMEANSHIST(K,ASGN) computes the histogram of
+        the IKM clusters activated by cluster assignments ASGN. """
+
+    h = np.zeros(K, 'float64')
+    h = vl_binsum(h, np.array([1], 'float64'), np.array(asgn, 'float64'))
+    return h
